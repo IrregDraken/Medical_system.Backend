f6acb11b5b5a02cee3c2b59e1b10cdea12dd5c8b
IrregDraken <allenfriday0@gmail.com>
2026-07-20 20:38:41 +0100

    Use Railway PORT environment variable

--- app.py
+++ app.py
@@ -793,9 +793,13 @@ def debug_users():
 # RUN APP
 # ==========================================
 
+import os
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        debug=True,
         host="0.0.0.0",
-        port=5000
+        port=port,
+        debug=False
     )
